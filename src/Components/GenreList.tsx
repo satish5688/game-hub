a9af6d4d89c4d7a168ba8fb0072d5ebdbe656649
Satish5688 <satishmungusmare5688@gmail.com>
2024-08-20 13:56:38 +0530

    clean up the genres

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, Heading, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "./hooks/useGenres";
 import getCropedImageUrl from "./image-url";
 
@@ -12,6 +12,8 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   const skeletons = [1, 2, 4, 5, 6, 7, 8, 3, 9, 10];
 
   return (
+    <>
+    <Heading fontWeight={'bold'} fontSize={'2xl'} marginBottom={3}>Genres</Heading>
     <List>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY={"5px"}>
@@ -19,9 +21,12 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
             <Image
               boxSize={"35px"}
               borderRadius={8}
+              objectFit={'cover'}
               src={getCropedImageUrl(genre.image_background)}
             ></Image>
             <Button
+              whiteSpace={'normal'}
+              textAlign={"left"}
               variant="link"
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectedGenre(genre)}
@@ -33,6 +38,7 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
         </ListItem>
       ))}
     </List>
+    </>
   );
 };
 
